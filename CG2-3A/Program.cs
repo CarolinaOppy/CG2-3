dc0d97110004d0a539643ae817cf6003ca1c5b0f
carocosta87 <carocosta87@gmail.com>
2018-07-25 20:20:23 -0500

    Added Code

--- CG2-3A/Program.cs
+++ CG2-3A/Program.cs
@@ -6,7 +6,19 @@ namespace CG2_3A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Carolina Costansa Oppy.");
+
+            Console.Write("What is your first name? ");
+            String firstName = Console.ReadLine();
+
+            Console.Write("What is your middle name? ");
+            String middleName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            String lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + firstName + " " + middleName + " " + lastName + "!");
+            Console.ReadLine();
         }
     }
 }
